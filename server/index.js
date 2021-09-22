b974dca554342d974e056111499fee5d99a676bf
Jobert Manosca <joberttm@hotmail.com>
2021-09-22 12:26:17 -0400

    Set up basic node backend

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// allows you to call environment variables
+require('dotenv').config();
+
+app.use(cors()); // allows cross-origin requests
+app.use(express.json()); // allows you to pass json payloads from frontend to backend
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
