0984270bd84ef869382be8783d50ad38a391b085
Jobert Manosca <joberttm@hotmail.com>
2021-09-22 17:37:51 -0400

    Set up back end login and sign up

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const authRoutes = require('./routes/auth');
+
 // allows you to call environment variables
 require('dotenv').config();
 
@@ -14,5 +16,7 @@ app.use(express.urlencoded());
 app.get('/', (req, res) => {
   res.send('hello');
 });
+
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
