35b85c38d19d3550378bbaaaf7056b508e67fc43
Jobert Manosca <joberttm@hotmail.com>
2021-09-23 15:04:28 -0400

    App displays user messages in general channel

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { Channel, useChatContext, MessageTeam } from 'stream-chat-react'
 
-const ChannelContainer = () => {
+import { ChannelInner, CreateChannel, EditChannel } from './';
+
+const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, createType }) => {
+  // Need to get info about a specific channel
+
+  const { channel } = useChatContext();
+
+  if(isCreating) {
+    return (
+      <div className="channel__container">
+        <CreateChannel createType={createType} setIsCreating={setIsCreating} />
+      </div>
+    );
+  }
+
+  if(isEditing) {
+    return (
+      <EditChannel setIsEditing={setIsEditing} />
+    );
+  }
+
+  const EmptyState = () => (
+    <div className="channel-empty__container">
+      <p className="channel-empty__first">This is the beginning of your chat history.</p>
+      <p className="channel-empty__second">Send messages, attachments, links, emojis, and more!</p>
+    </div>
+  );
+
   return (
-    <div>
-      ChannelContainer
+    <div className="channel__container">
+      <Channel
+        EmptyStateIndicator={EmptyState}
+        Message={(messageProps, i) => <MessageTeam key={i} {...messageProps} />}
+      >
+        <ChannelInner setIsEditing={setIsEditing}/>
+      </Channel>
     </div>
   )
 }
